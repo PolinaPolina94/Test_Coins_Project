c2b100d1648376ef3e99f7a35a4cdba332ae1c76
Полина <polina_kazhina@mail.ru>
2022-12-12 23:05:11 +0300

    fix: fixed a little with prettier

--- src/redux/modal-reducer.js
+++ src/redux/modal-reducer.js
@@ -16,31 +16,31 @@ const ModalReducer = (state = initialState, action) => {
         ...state,
         isOpen: true,
         name: action.name,
-        price: action.price
+        price: action.price,
       };
     case CLOSE_MODAL:
       return {
         ...state,
         isOpen: false,
       };
-      case INCREMENT_PRICE:
-        return {
-        ...state, 
-        price: action.price++   
-        };
-        case DECREMENT_PRICE:
-        return {
-          ...state,
-          price: action.price-- 
-        };
-        
+    case INCREMENT_PRICE:
+      return {
+        ...state,
+        price: action.price++,
+      };
+    case DECREMENT_PRICE:
+      return {
+        ...state,
+        price: action.price--,
+      };
+
     default:
       return state;
   }
 };
 
 export const showModal = (isOpen, name, price) => {
-  return { type: SHOW_MODAL, isOpen, name, price};
+  return { type: SHOW_MODAL, isOpen, name, price };
 };
 
 export const closeModal = (isOpen) => {
@@ -48,11 +48,11 @@ export const closeModal = (isOpen) => {
 };
 
 export const incrementPrice = (price) => {
-  return { type: INCREMENT_PRICE, price};
+  return { type: INCREMENT_PRICE, price };
 };
 
 export const decrementPrice = (price) => {
-  return { type: DECREMENT_PRICE, price};
+  return { type: DECREMENT_PRICE, price };
 };
 
 export default ModalReducer;
